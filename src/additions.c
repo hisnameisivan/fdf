37f868445ec37b275ff845eceaa6205cf4ab20a5
hisnameisivan <hisnameisivan@ya.ru>
2020-03-04 01:01:17 +0300

    src/additions.c - check mistake (holy pointers)

--- src/additions.c
+++ src/additions.c
@@ -6,24 +6,26 @@
 /*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 22:55:07 by waddam            #+#    #+#             */
-/*   Updated: 2020/03/03 23:02:56 by waddam           ###   ########.fr       */
+/*   Updated: 2020/03/04 00:35:48 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
+// #include "../includes/fdf.h"
 
-void	free_multiline(char **multiline)
+void	free_multiline(char ***multiline)
 {
 	int		i;
 
 	i = 0;
-	if (multiline)
+	if (*multiline)
 	{
-		while (multiline[i])
+		while ((*multiline)[i])
 		{
-			free(multiline[i]);
+			free((*multiline)[i]);
 			i++;
 		}
+		free(*multiline);
+		*multiline = NULL;
 	}
-	free(multiline);
 }
