c63daf20c8c824be93ec4d92adcb537a5a90d2d6
hisnameisivan <hisnameisivan@ya.ru>
2020-03-04 01:07:30 +0300

    src/read_map.c - change return type, del cnt_pnts, add free, add check res

--- src/read_map.c
+++ src/read_map.c
@@ -6,29 +6,32 @@
 /*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 00:11:13 by waddam            #+#    #+#             */
-/*   Updated: 2020/03/01 23:12:11 by waddam           ###   ########.fr       */
+/*   Updated: 2020/03/04 00:51:42 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
+// #include "../includes/fdf.h"
 
-int			read_map(int fd, t_map *map)
+t_point		*read_map(int fd, t_map *map)
 {
 	int		res;
 	char	*line;
 	char	**z_line;
-	t_point	*begin;
-	int		cnt_pnts;
+	t_point	*points;
 
-	cnt_pnts = 0;
 	line = NULL;
 	while ((res = get_next_line(fd, &line)) > 0)
 	{
 		if (!(z_line = ft_strsplit(line, ' ')))
 			break ;
-		if (parsing(z_line, &begin, &cnt_pnts, map) == -1)
-			return (-1);
+		if (parsing(z_line, &points, map) == -1)
+			return (NULL);
+		map->height++;
+		free_multiline(&z_line);
+		ft_strdel(&line);
 	}
-	free(line);
-	return (res);
+	if (res < 0 || map->width == 0)
+		return (NULL);
+	return (points);
 }
